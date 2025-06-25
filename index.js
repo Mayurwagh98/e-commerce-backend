589acbba2d85b49c3c057d1d2860347158e77de9
mayur wagh <mswagh98@gmail.com>
2025-06-25 21:14:24 +0530

    add: basic code for server

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const connectDB = require("./config/database");
+require("dotenv").config();
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+connectDB()
+  .then(() => {
+    console.log("connected to database");
+    app.listen(8000, () => {
+      console.log("server is running on port 8000");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });

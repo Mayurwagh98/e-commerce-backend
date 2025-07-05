ca37d8e1113618e92d7d359dda7ee9fdca738038
mayur wagh <mswagh98@gmail.com>
2025-07-05 16:12:37 +0530

    add: profile update api

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { userProfile } = require("../controller/user.controller");
+const { userProfile, updateProfile } = require("../controller/user.controller");
 const userAuth = require("../middlewares/auth.middleware");
 
 router.get("/myprofile", userAuth, userProfile);
+router.patch("/update-my-profile", userAuth, updateProfile);
 
 module.exports = router;
